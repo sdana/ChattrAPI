bd4687d0b22c157981d53fe05c10d195bd294952
Seth Dana <seth.dana@gmail.com>
2018-10-30 14:03:00 -0500

    Added Hubs/ and ChatHub.cs and got socket connection with CORS working.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChattrApi.Hubs
+{
+    [EnableCors("CorsPolicy")]
+    public class ChatHub : Hub
+    {
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+    }
+}
